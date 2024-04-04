6377fd6e3863369ecd8abefe31ab429cb92bca41
Liza-Aleks1108 <lizaaleks2005@gmail.com>
2024-04-04 13:58:16 +0000

    Deploying to gh-pages from @ Liza-Aleks1108/goit-js-hw-11@875b2a179079886d644d46622bfb10b124d94b9e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,15 +1,15 @@
-import{i as f,S as m}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();//! У файлі render-functions.js - функції для відображення елементів інтерфейсу.
-function d(o){return o.map(({largeImageURL:t,webformatURL:s,tags:n,likes:e,views:r,comments:i,downloads:u})=>`<li>
+import{i as f,S as p}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();//! У файлі render-functions.js - функції для відображення елементів інтерфейсу.
+function h(o){return o.map(({largeImageURL:t,webformatURL:i,tags:n,likes:e,views:r,comments:s,downloads:u})=>`<li>
     <a href="${t}">
-        <img src="${s}" alt="${n}"/>
+        <img src="${i}" alt="${n}" width="360" height="155"/>
     </a>
-        <ul>
-            <li>Likes ${e}</li>
-            <li>Views ${r}</li>
-            <li>Comments ${i}</li>
-            <li>Downloads ${u}</li>
+        <ul class="characteristics">
+            <li>Likes<br> <span class="span">${e}</span></li>
+            <li>Views<br> <span class="span">${r}</span></li>
+            <li>Comments<br> <span class="span">${s}</span></li>
+            <li>Downloads<br> <span class="span">${u}</span></li>
         </ul>
     </li>`).join("")}//! У файлі pixabay-api.js зберігаються функції для HTTP-запитів.
-const h="43217823-e472439c26018cf28ab0cff6b";function p(){const o=new URLSearchParams({key:h,q:l.value.trim(),image_type:"photo",orientation:"horizontal",safesearch:!0});c.innerHTML="",fetch(`https://pixabay.com/api/?${o}`).then(t=>{if(!t.ok)throw Error(t.status);return t.json()}).then(t=>{console.log("data",t),c.insertAdjacentHTML("beforeend",d(t.hits)),L.refresh(),t.hits.length===0&&f.show({message:"Sorry, there are no images matching your search query. Please try again!",color:"red",position:"topRight"})}).catch(t=>alert(t)).finally(()=>{S()})}//! У файлі main.js знаходиться вся логіка роботи додатка.
-const y=document.querySelector(".search-form"),a=document.querySelector(".loader"),l=document.querySelector(".search-input"),c=document.querySelector("ul");function g(o){if(o.preventDefault(),l.value.trim()===""){console.log("Empty string");return}b(),p()}const L=new m(".list a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,overlayOpacity:.8});y.addEventListener("submit",g);function b(){a.classList.remove("none")}function S(){a.classList.add("none")}
+const m="43217823-e472439c26018cf28ab0cff6b";function d(){const o=new URLSearchParams({key:m,q:l.value.trim(),image_type:"photo",orientation:"horizontal",safesearch:!0});a.innerHTML="",fetch(`https://pixabay.com/api/?${o}`).then(t=>{if(!t.ok)throw Error(t.status);return t.json()}).then(t=>{console.log("data",t),a.insertAdjacentHTML("beforeend",h(t.hits)),b.refresh(),t.hits.length===0&&f.show({message:"Sorry, there are no images matching your search query. Please try again!",color:"red",position:"topRight"})}).catch(t=>alert(t)).finally(()=>{S()})}//! У файлі main.js знаходиться вся логіка роботи додатка.
+const y=document.querySelector(".search-form"),c=document.querySelector(".loader"),l=document.querySelector(".search-input"),a=document.querySelector("ul");function g(o){if(o.preventDefault(),l.value.trim()===""){console.log("Empty string");return}L(),d()}const b=new p(".list a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,overlayOpacity:.8});y.addEventListener("submit",g);function L(){c.classList.remove("none")}function S(){c.classList.add("none")}
 //# sourceMappingURL=commonHelpers.js.map
